0f1208d88ee844b5232a3db08fe738ff78e957de
yanisounas <106371569+yanisounas@users.noreply.github.com>
2023-02-05 23:44:13 +0100

    Bugfix
    Includes directly a file if it exists. mime_content_type on a css file returns text/plain so a check is necessary

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php
 //TODO: Exceptions code and messages
+if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"]))
+{
+    if (array_slice(explode('.', $_SERVER["REQUEST_URI"]), -1)[0] == "css")
+        header("Content-Type: text/css; charset=utf-8");
+    else
+        header(sprintf("Content-Type: %s;", mime_content_type($_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"])));
+
+    include $_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"];
+    die;
+}
+
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
 
 $router = new \FastFramework\Router\Router();
